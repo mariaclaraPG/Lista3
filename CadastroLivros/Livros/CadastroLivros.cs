9d4d94c6e94702f8615a6f0aa1f5cc88809ed81f
mariaclaraPG <maria3.goncalves@alunos.ifsuldeminas.edu.br>
2025-11-28 21:54:54 -0300

    Add files via upload

---
+++ CadastroLivros/Livros/CadastroLivros.cs
@@ -0,0 +1,11 @@
+namespace CadastroLivros
+{
+    class Livro
+    {
+        public string titulo;
+        public string autor;
+        public int ano;
+        public int prateleira;
+    }
+
+}
